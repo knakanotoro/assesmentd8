ad0177b959e9ab76b320072df84727d085b3725c
Kenji <k.nakano.toro@gmail.com>
2020-04-03 00:34:00 +0900

    Translations

---
+++ web/sites/default/files/languages/fr_T0sG7aQPizSo1AgcC4GpwYnYDaUhY5paPy63q94h_2U.js
@@ -0,0 +1,1 @@
+window.drupalTranslations = {"strings":{"":{"Link":"Lien","Image":"Image","Unlink":"Supprimer le lien","Edit Link":"Lien de modification"}},"pluralFormula":{"0":0,"1":0,"default":1}};
---
+++ web/sites/default/files/languages/ja_CHZXau4pYeGvFhYr-l2fgDAbiGT0I0B5-wozWOIevUY.js
@@ -0,0 +1,1 @@
+window.drupalTranslations = {"strings":{"":{"Link":"\u30ea\u30f3\u30af","Image":"\u753b\u50cf","Unlink":"\u30ea\u30f3\u30af\u3057\u306a\u3044","Edit Link":"\u30ea\u30f3\u30af\u306e\u7de8\u96c6"}},"pluralFormula":{"1":0,"default":1}};
